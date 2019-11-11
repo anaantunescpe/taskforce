03b46b4bd8a18c48db89c40d6fe1638320eb6d1e
Bernardo <bernardoandrade@cpejr.com.br>
2019-11-11 16:19:30 -0300

    Database and firebase Setup

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const firebase = require('firebase');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const cons = require('consolidate');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var cons = require('consolidate');
+const app = express();
 
-var app = express();
+/**
+ *  Database setup
+ */
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_SERVER}/${process.env.MONGO_DATABASE}?${process.env.MONGO_OPTIONS}`);
+mongoose.connection.on('error', console.error.bind(console, 'connection error: '));
+mongoose.connection.once('open', () => {
+  console.log('Database connect!')
+});
+
+
+/**
+ * firebase setup
+ */
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
 
 // view engine setup
 app.engine('html', cons.swig)
@@ -15,8 +42,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const firebase = require('firebase');
+const Blog = require('../models/blog');
 var router = express.Router();
 
 /* GET home page. */
@@ -12,6 +14,26 @@ router.get('/error', (req, res, next) => {
 
 router.get('/about', (req, res, next) => {
   res.render('about', {title: 'Quem Somos!'})
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login', {title: 'Acesso Restrito'})
+});
+
+router.post('/login', (req, res, next) => {
+  const user = req.body.user
+  console.log(user);
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password).then((fIREBASE) => {
+    console.log("_________________________________________________________________________________________________");
+    console.log(fIREBASE);
+    console.log("_________________________________________________________________________________________________");
+    console.log(fIREBASE.user.uid);
+    console.log("_________________________________________________________________________________________________");
+    res.redirect('/')
+  }).catch((error) => {
+    console.log(error);
+    res.redirect('/error');
+  });
 });
 
 module.exports = router;
